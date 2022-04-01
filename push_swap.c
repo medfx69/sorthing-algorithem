48009254f0715ec2a879e35f4cbc25fcc7757abf
Mohamed Ait aadi <mait-aad@e2r7p1.1337.ma>
2022-04-01 19:53:36 +0000

    g

--- push_swap.c
+++ push_swap.c
@@ -6,25 +6,92 @@
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:09:23 by mait-aad          #+#    #+#             */
-/*   Updated: 2022/03/30 13:56:36 by mait-aad         ###   ########.fr       */
+/*   Updated: 2022/04/01 18:54:53 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_sort(char **c_n, int	*n, int len)
+void	sent_stack2(int *list1, int	*list2, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = list2[i + 1];
+		list2[len] = list2[len - 1];
+		len--;
+	}
+	list2[0] = list1[0];
+	write(1, "pa\n", 3);
+}
+
+void	ft_moves(int *list1, int	*list2, int deg, int len)
+{
+	int	i;
+
+	i = 0;
+	while (list1[i + 1])
+	{
+		while (deg < 1)
+		{
+			if (list1[i] / deg < list1[i + 1] / deg)
+				sent_stack2(list1, list2, len--);
+			else if (list1[i] / deg > list1[i + 1] / deg)
+			{
+				chan_f_s(list1);
+				sent_stack2(list1, list2, len--);
+			}
+			else
+				deg = deg / 10;
+		}
+	}
+}
+
+int	finde_degree(int *list)
+{
+	int	i;
+	int	max;
+	int	degree;
+
+	i = 0;
+	max = 0;
+	while (list[i])
+	{
+		if (list[i] > max)
+			max = list[i];
+		i++;
+	}
+	i = 0;
+	while (max > 9)
+	{
+		max / 10;
+		i++;
+	}
+	max = 0;
+	degree = 1;
+	while (max++ < i)
+		degree = degree * 10;
+	return (degree);
+}
+
+void	ft_sort(char **c_n, int	*stack1, int len)
 {
 	int	j;
+	int	*stack2;
+	int	degree;
 
 	j = 0;
 	while (j < len)
 	{
-		n[j] = ft_atoi(c_n[j]);
+		stack1[j] = ft_atoi(c_n[j]);
 		j++;
 	}
-	j = 0;
-	while (j < len)
-		printf("%d\n", n[j++]);
+	stack2 = (int *)malloc(sizeof(int) * len);
+	degree = finde_degree(stack1);
+	ft_moves(stack1, stack2, degree, len);
 }
 
 int	main(int ac, char	**av)
@@ -33,9 +100,6 @@ int	main(int ac, char	**av)
 	int		*nums;
 	int		i;
 
-	chr_nums = ft_split(av [1], ' ');
-	while (chr_nums[i])
-		i++;
-	nums = (int *)malloc(sizeof(int) * i);
-	ft_sort(chr_nums, nums, i);
+	nums = (int *)malloc(sizeof(int) * (ac - 1));
+	ft_sort(av + 1, nums, ac - 1);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:32:11 by mait-aad          #+#    #+#             */
-/*   Updated: 2022/03/30 13:50:05 by mait-aad         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:50:57 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 
 int		ft_atoi(const char	*str);
 char	*ft_strjoin(char const	*s1, char const	*s2);
-char	**ft_split(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
