b23fe6b0b19ff3b353ff8f8b3bc54e478360cb4b
Mohamed Ait aadi <mait-aad@e1r3p9.1337.ma>
2022-04-06 22:54:31 +0000

    k

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:09:23 by mait-aad          #+#    #+#             */
-/*   Updated: 2022/04/06 17:08:59 by mait-aad         ###   ########.fr       */
+/*   Updated: 2022/04/06 22:19:55 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	sent_stack2(int *list1, int	*list2, int len, int len_2)
 {
 	if (list1[0] > list1[1])
+	{
 		chan_f_s(list1);
+		write(1, "sa\n", 3);
+	}
 	push_list1(list2, len_2);
 	list2[0] = list1[0];
 	mov_f(list1, len);
-	//write(1, "pa\n", 3);
+	write(1, "pb\n", 3);
 	if (list2[0] < list2[1])
 	{
 		chan_f_s(list2);
-		//write (1, "m\n", 3);
+		write (1, "sb\n", 3);
 	}
 }
 
@@ -32,9 +35,13 @@ void	return_to_stack1(int	*list1, int	*list2, int len, int len_2)
 	while (len_2 > 0)
 	{
 		if (list2[0] < list2[1])
+		{
 			chan_f_s(list2);
+			write(1, "sb\n", 3);
+		}
 		push_list1(list1, len);
 		list1[0] = list2[0];
+		write(1, "pa\n", 3);
 		mov_f(list2, len_2);
 		len++;
 		len_2--;
@@ -62,11 +69,10 @@ void	ft_moves(int *list1, int	*list2, int deg, int len)
 			j = j / 10;
 		}
 	}
-	//printf("<%d><%d>\n",len , i);
 	return_to_stack1(list1, list2, len, i);
 }
 
-void	ft_sort(char **c_n, int	*stack1, int len)
+int	ft_sort(char **c_n, int	*stack1, int len)
 {
 	int	j;
 	int	*stack2;
@@ -78,22 +84,21 @@ void	ft_sort(char **c_n, int	*stack1, int len)
 		stack1[j] = ft_atoi(c_n[j]);
 		j++;
 	}
+	if (check_nums(stack1, len) < 0)
+		return (-1);
 	stack2 = (int *)malloc(sizeof(int) * len);
 	if (stack2 == NULL)
-		return ;
+		return (0);
 	ft_bzero(stack2, sizeof(int) * len);
 	degree = finde_degree(stack1, len);
 	j = 0;
-	while (j + 1 < len)
+	while (j < len)
 	{
 		if (stack1[j] > stack1[j + 1])
 			ft_moves(stack1, stack2, degree, len);
-		printf("%d\n", j);
 		j++;
 	}
-	j = 0;
-	while (j < len)
-		printf("%d\n",stack1[j++]);
+	return (*stack1);
 }
 
 int	main(int ac, char	**av)
--- utilites_3.c
+++ utilites_3.c
@@ -1,48 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   linkedlist2.c                                      :+:      :+:    :+:   */
+/*   utilites_3.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 16:15:45 by mait-aad          #+#    #+#             */
-/*   Updated: 2022/04/05 16:16:48 by mait-aad         ###   ########.fr       */
+/*   Created: 2022/04/06 22:20:31 by mait-aad          #+#    #+#             */
+/*   Updated: 2022/04/06 22:24:59 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_list	*ft_lstlast(t_list *lst)
+int	check_nums(int	*list, int len)
 {
-	if (lst)
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
 	{
-		while (lst->next != NULL)
-			lst = lst->next;
+		j = 0;
+		while (j < len)
+		{
+			if (list[i] == list[j] && j != i)
+				return (-1);
+			j++;
+		}
+		i++;
 	}
-	return (lst);
-}
-
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*str;
-
-	str = (t_list *)malloc(sizeof(t_list));
-	if (!str)
-		return (NULL);
-	str->content = content;
-	str->next = NULL;
-	return (str);
-}
-
-int	ft_lstsize(t_list *lst)
-{
-	int	count;
-
-	count = 0;
-	while (lst != NULL)
-	{
-		count++;
-		lst = lst->next;
-	}
-	return (count);
+	return (1);
 }
